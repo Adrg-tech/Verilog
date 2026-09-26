cd82fd43a04c68d881ccde58f437780b2b780b5a
AR <233392886+Adrg-tech@users.noreply.github.com>
2026-09-26 02:25:18 -0700

    Refactor SR flip-flop testbench clock generation

--- sr_ff/srff_tb.v
+++ sr_ff/srff_tb.v
@@ -16,18 +16,21 @@ srff dut(
 );
 
 initial begin
+    clk=0;
+    forever #5 clk=~clk;
+end
+
+initial begin
     $dumpfile("srff.vcd");
     $dumpvars(0,srff_tb);
     for (i=0;i<2;i=i+1) begin
         for (j=0;j<2;j=j+1) begin
-            for (k=0;k<2;k=k+1) begin
-                s=i;
-                r=j;
-                clk=k;
-                #10;
-            end
+            s=i;
+            r=j;
+            #10;
         end
     end
+    $finish;
 end
 
 endmodule;
